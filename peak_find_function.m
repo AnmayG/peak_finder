b53d8c1c7d300df573617e4a8658f3a765b4ead9
Anmay Gupta <anmaygupta1@gmail.com>
2024-05-29 18:09:27 -0500

    fixed nickelate

--- peak_find_function.m
+++ peak_find_function.m
@@ -1,5 +1,5 @@
 function seed = peak_find_function(split, tolerance, x, y, gWide)
-        [vals, locs, widths, ~, threshold, signal, raw] = find_peaks_at_point(x, y, gWide, false, 2, 5, 1e8, 41, 6);
+        [vals, locs, widths, ~, threshold, signal, raw] = find_peaks_at_point(x, y, gWide, false, 2, 10, 5e8, 41, 7);
         [sorted_locs, sorted_vals, sorted_widths, pairs] = partner_peak(locs, split, tolerance, signal, threshold, gWide, vals, widths, false, raw);
         baseline = find_baseline(x, y, gWide);
         seed = peaks_to_seed(sorted_locs, sorted_vals, sorted_widths, pairs, baseline, split, tolerance);
