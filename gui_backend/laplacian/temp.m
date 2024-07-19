49a43fdfe6b67285ef3cfea25b72e4ca4ae4b4f2
Anmay Gupta <anmaygupta1@gmail.com>
2024-07-19 12:28:13 -0500

    various changes, mostly trying out all the different algos for partnering

--- gui_backend/laplacian/temp.m
+++ gui_backend/laplacian/temp.m
@@ -1,10 +1,12 @@
-function plot_point_pages(max_points, points)
+function plot_point_pages(max_points, points, name)
+    disp(name)
     figure;
     num_pages = max_points;
     intervals = zeros(max_points, 3);
     for k = 1:num_pages
         data = squeeze(points(:, :, k));
         data(data == 0) = NaN;
+        data = flipud(data);
         intervals(k, :) = [min(data(:)), max(data(:)), range(data(:))];
         subplot(ceil(sqrt(num_pages)), ceil(sqrt(num_pages)), k);
         imAlpha=ones(size(data));
@@ -15,25 +17,13 @@ function plot_point_pages(max_points, points)
         colorbar;
         xlabel('Pixel Index (Column)');
         ylabel(['Value of Point ' num2str(k)]);
-        title(['Points in Bucket ' num2str(k)]);
+        title([name 'Points in Bucket ,' num2str(k)]);
         axis equal;
         axis tight;
         hold off;
     end
 
     disp(intervals)
-    % for i=1:size(intervals, 1)
-    %     for j=i+1:size(intervals, 1)
-    %         if (intervals(i, 1) < intervals(j, 1) && intervals(i, 2) > intervals(j, 1)) || ...
-    %            (intervals(i, 1) < intervals(j, 2) && intervals(i, 2) > intervals(j, 2)) || ...
-    %            (intervals(i, 1) > intervals(j, 1) && intervals(i, 2) < intervals(j, 2)) || ...
-    %            (intervals(i, 1) < intervals(j, 1) && intervals(i, 2) > intervals(j, 2))
-    %             disp("Failure")
-    %             disp(intervals(i, :))
-    %             disp(intervals(j, :))
-    %         end
-    %     end
-    % end
 
     figure;
     subplot(1, 2, 1)
@@ -47,8 +37,10 @@ function plot_point_pages(max_points, points)
         color = colors(mod(i - 1, numel(colors)) + 1);
         scatter(data(:, i), 1:size(points, 1), 10, color, 'filled', 'MarkerEdgeColor', color)
     end
+    legend(["Bucket " i])
     set(gca, 'YDir','reverse')
-    title("Horizontal")
+    title([name "Horizontal"])
+    legend(["1" 2:size(points, 3)])
     hold off;
     subplot(1, 2, 2)
     hold on;
@@ -62,8 +54,23 @@ function plot_point_pages(max_points, points)
         scatter(data(:, i), 1:size(points, 1), 10, color, 'filled', 'MarkerEdgeColor', color)
     end
     set(gca, 'YDir','reverse')
-    title("Vertical")
+    title([name "Vertical"])
+    legend(["1" 2:size(points, 3)])
     hold off;
+
+    figure;
+    hold on; 
+    [~, cols] = size(points(:, :, 1));
+    linebreaks = cols:cols:numel(points(:, :, 1));
+    for i=1:size(points, 3) 
+        move = points(:, :, i);
+        move(move == 0) = NaN; 
+        % move = imgaussfilt(move, 3);
+        plot(move(:), "o", "DisplayName", ['Slice ' num2str(i)]); 
+    end 
+    axis tight;
+    xline(linebreaks, ':', "HandleVisibility", 'off'); 
+    legend();
 end
 
 f = waitbar(0, "Initializing");
@@ -76,7 +83,7 @@ i = 1;
 for k=1:max_points
     if all(locs_array(:, :, k) == 0)
         points(:, :, i) = [];
-    else    
+    else
         i = i + 1;
         max_points = max_points - 1;
     end
@@ -138,12 +145,48 @@ for k=(length(mins)+1):length(all_points) % for each peak after the first pixel
     clusters(k) = group;
 end
 
+first_el = squeeze(points(1, 1, :));
+last_el = squeeze(points(180, 180, :));
+minsi = zeros(max_points, 1);
+minsi2 = last_el;
+clustered_points = zeros(size(points));
+clustered_points2 = zeros(size(points));
+for i=1:size(points, 1)
+    for j=1:size(points, 2)
+        locs = squeeze(points(i, j, :));
+        dists = pdist2(locs, minsi);
+        pairs = matchpairs(dists, 3e8);
+        clustered_points(i, j, pairs(:, 1)) = locs(pairs(:, 2));
+        if i == 90
+            disp([i, j, pairs(:, 1)'])
+            disp([minsi, locs])
+            disp(pairs)
+        end
+        minsi(pairs(:, 2)) = locs(pairs(:, 2));
+    end
+end
+
+for i=size(points, 1):-1:1
+    for j=size(points, 2):-1:1
+        locs = squeeze(points(i, j, :));
+        dists = pdist2(locs, minsi2);
+        pairs = matchpairs(dists, 3e8);
+        clustered_points2(i, j, pairs(:, 2)) = locs(pairs(:, 1));
+        minsi2 = locs;
+    end
+end
+
+plot_point_pages(max_points, points, "Original")
+plot_point_pages(max_points, clustered_points, "Forwards")
+% plot_point_pages(max_points, clustered_points2, "Backwards")
+
 last_el = squeeze(points(180, 180, :));
 mins2 = last_el; % Contains the minimums for each group
 groups2 = arrayfun(@(x) {x}, last_el(:));
 clusters2 = zeros(length(all_points), 1);
 clusters2((length(all_points)-length(last_el) + 1):length(all_points)) = 1:length(last_el);
 curr_pixel = true(max_points, 1);
+curr_dists = inf * ones(max_points, 1);
 curr_pixel_x = size(points, 1);
 curr_pixel_y = size(points, 2);
 for k = (length(all_points) - length(mins2)):-1:1 % for each peak after the first pixel
@@ -154,48 +197,49 @@ for k = (length(all_points) - length(mins2)):-1:1 % for each peak after the firs
         curr_pixel_x = i;
         curr_pixel_y = j;
         curr_pixel = true(max_points, 1);
+        curr_dists = inf * ones(max_points, 1);
     end
     
     % Find the closest element in mins2 to the current peak in all_points
     dist = abs(mins2 - all_points(k));
-    dist(~curr_pixel) = inf; % Set distances of already used groups to infinity
-    [~, group] = min(dist);
+    % disp("Start")
+    % disp([dist, mins2, curr_dists])
+    dist(dist >= curr_dists) = inf;
+    % disp([dist; all_points(k)])
+    % dist(~curr_pixel) = inf; % Set distances of already used groups to infinity
+    [val, group] = min(dist);
     
     % Update the group and mark it as used
     curr_pixel(group) = false;
+    curr_dists(group) = val;
     mins2(group) = all_points(k);
     groups2{group} = [groups2{group}, all_points(k)];
     clusters2(k) = group;
-    
-    if i == 90
-        disp([abs(mins2 - all_points(k))', all_points(k)]);
-        disp([curr_pixel_y, group, k, curr_pixel']);
-    end
 end
 
-figure;
-hold on;
-colors = ['r', 'g', 'b', 'c', 'm', 'k'];
-for k=1:length(all_points) % Horizontal line cut
-    if pixel_indices(k, 1) == 90
-        % disp([pixel_indices(k, 1), pixel_indices(k, 2), clusters(k)])
-        scatter(all_points(k), pixel_indices(k, 2), colors(clusters(k)))
-    end
-end
-set(gca, 'YDir','reverse')
-
-figure;
-hold on;
-colors = ['r', 'g', 'b', 'c', 'm', 'k'];
-for k=1:length(all_points) % Horizontal line cut
-    if pixel_indices(k, 1) == 90
-        % disp([pixel_indices(k, 2), clusters2(k)])
-        % disp([pixel_indices(k, 1), pixel_indices(k, 2), clusters(k)])
-        scatter(all_points(k), pixel_indices(k, 2), colors(clusters2(k)))
-    end
-end
-set(gca, 'YDir','reverse')
-disp([mins, mins2])
+% figure;
+% hold on;
+% colors = ['r', 'g', 'b', 'c', 'm', 'k'];
+% for k=1:length(all_points) % Horizontal line cut
+%     if pixel_indices(k, 1) == 90
+%         % disp([pixel_indices(k, 1), pixel_indices(k, 2), clusters(k)])
+%         scatter(all_points(k), pixel_indices(k, 2), colors(clusters(k)))
+%     end
+% end
+% set(gca, 'YDir','reverse')
+% 
+% figure;
+% hold on;
+% colors = ['r', 'g', 'b', 'c', 'm', 'k'];
+% for k=1:length(all_points) % Horizontal line cut
+%     if pixel_indices(k, 1) == 90
+%         % disp([pixel_indices(k, 2), clusters2(k)])
+%         % disp([pixel_indices(k, 1), pixel_indices(k, 2), clusters(k)])
+%         scatter(all_points(k), pixel_indices(k, 2), colors(clusters2(k)))
+%     end
+% end
+% set(gca, 'YDir','reverse')
+% disp([mins, mins2])
 
 % seeds = zeros(max_points, 1);
 % for k=1:max_points
@@ -203,59 +247,59 @@ disp([mins, mins2])
 % end
 % [cluster_indices, cluster_centers] = kmeans(all_points, max_points,"Distance","cityblock", "Start",seeds);
 
-waitbar(7/8, f, "Assigning");
-bucketed_points = zeros(size(points));
-num_dupes = ones(max_points);
-icache = 1;
-jcache = 1;
+% waitbar(7/8, f, "Assigning");
+% bucketed_points = zeros(size(points));
+% num_dupes = ones(max_points);
+% icache = 1;
+% jcache = 1;
 % Assign clustered points back to their respective pixels and buckets
-for k = 1:length(all_points)
-    i = pixel_indices(k, 1);
-    j = pixel_indices(k, 2);
-    if i ~= icache || j ~= jcache % If change in pixel reset dupes
-        icache = i;
-        jcache = j;
-        num_dupes = ones(max_points);
-    end
+% for k = 1:length(all_points)
+%     i = pixel_indices(k, 1);
+%     j = pixel_indices(k, 2);
+%     if i ~= icache || j ~= jcache % If change in pixel reset dupes
+%         icache = i;
+%         jcache = j;
+%         num_dupes = ones(max_points);
+%     end
+% 
+%     bucket_idx = cluster_indices(k);
+%     val = all_points(k);
+%     if bucketed_points(i, j, bucket_idx) ~= 0 % If cluster dupe
+%         Store the average
+%         temp_total = val * num_dupes(bucket_idx) + bucketed_points(i, j, bucket_idx);
+%         num_dupes(bucket_idx) = num_dupes(bucket_idx) + 1;
+%         val = temp_total / num_dupes(bucket_idx);
+%     end
+%     fails = false(size(cluster_centers));
+%     while bucketed_points(i, j, bucket_idx) ~= 0
+%         fails(bucket_idx) = true;
+%         if all(fails)
+%             break
+%         end
+%         distances = abs(cluster_centers - all_points(k));
+%         distances(fails) = inf;
+%         [~, bucket_idx] = min(distances);
+%     end
+%     bucketed_points(i, j, bucket_idx) = val;
+% end
+% 
+% mins = zeros(max_points, 1);
+% for k = 1:max_points
+%     data = squeeze(bucketed_points(:, :, k));
+%     data(data == 0) = NaN;
+%     mins(k) = min(data(:));    
+% end
+% [sorted_mins, indices] = sort(mins);
+% 
+% sorted_bucketed_points = zeros(image_size(1), image_size(2), max_points);
+% for k = 1:max_points
+%     sorted_bucketed_points(:, :, k) = bucketed_points(:, :, indices(k));
+% end
 
-    bucket_idx = cluster_indices(k);
-    val = all_points(k);
-    if bucketed_points(i, j, bucket_idx) ~= 0 % If cluster dupe
-        % Store the average
-        temp_total = val * num_dupes(bucket_idx) + bucketed_points(i, j, bucket_idx);
-        num_dupes(bucket_idx) = num_dupes(bucket_idx) + 1;
-        val = temp_total / num_dupes(bucket_idx);
-    end
-    % fails = false(size(cluster_centers));
-    % while bucketed_points(i, j, bucket_idx) ~= 0
-    %     fails(bucket_idx) = true;
-    %     if all(fails)
-    %         break
-    %     end
-    %     distances = abs(cluster_centers - all_points(k));
-    %     distances(fails) = inf;
-    %     [~, bucket_idx] = min(distances);
-    % end
-    bucketed_points(i, j, bucket_idx) = val;
-end
+% waitbar(7/8, f, "Plotting");
+% disp("old")
+% plot_point_pages(max_points, points, "Old")
 
-mins = zeros(max_points, 1);
-for k = 1:max_points
-    data = squeeze(bucketed_points(:, :, k));
-    data(data == 0) = NaN;
-    mins(k) = min(data(:));    
-end
-[sorted_mins, indices] = sort(mins);
-
-sorted_bucketed_points = zeros(image_size(1), image_size(2), max_points);
-for k = 1:max_points
-    sorted_bucketed_points(:, :, k) = bucketed_points(:, :, indices(k));
-end
-
-waitbar(7/8, f, "Plotting");
-disp("old")
-plot_point_pages(max_points, points)
-
-disp("new")
-plot_point_pages(max_points, sorted_bucketed_points)
+% disp("new")
+% plot_point_pages(max_points, sorted_bucketed_points, "new")
 close(f)
