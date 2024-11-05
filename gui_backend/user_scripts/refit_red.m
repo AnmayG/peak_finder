e96079714ca8fcfb645d759e640871cc99334b9b
Anmay Gupta <anmaygupta1@gmail.com>
2024-11-04 23:31:22 -0600

    readd plotting fit preview

---
+++ gui_backend/user_scripts/refit_red.m
@@ -0,0 +1,29 @@
+function function_outputs = refit_red(function_inputs)
+    %   function_inputs = struct array with fields:
+    %       signal: [n × n × freq double]
+    %   SweepParam: freq
+    %   parameters: [n × n × 17 double]
+    %    peak_info: [1×1 struct] with fields locs, vals, widths, proms
+    %   reflection: [n × n double]
+    %     settings: [1×1 struct] with each parameter from inputs
+    function_outputs = function_inputs;
+    params = function_inputs(1).settings(1);
+    % Modify function_outputs fields below
+    num_inputs = length(function_inputs);
+    for i=1:num_inputs
+        in = function_inputs(i);
+        mask = in.settings(i).roi_mask;
+        prev_params = in.parameters;
+        for x=1:size(mask,1)
+            for y=1:size(mask,2)
+                if mask(y,x) && prev_params(y, x, 1) ~= 0 % D map is nonzero
+                    mask(y,x) = 0;
+                end
+            end
+        end
+        [pdf, ~, peak_info] = full_peak_wrapper([], in.signal, in.SweepParam, ...
+            in.settings(i), 0, in.reflection, 16);
+        function_outputs(i).parameters = pdf;
+        function_outputs(i).peak_info = peak_info;
+    end
+end
